f592f50c526ad583f0ebd3e4ab7e635ac6631656
Gauri Prakash Tanaji <prakashtanaji@Tanajis-MacBook-Pro.local>
2019-10-22 20:19:47 -0700

    anagrams, also handle duplicates

--- dailycode/String/anagrams.java
+++ dailycode/String/anagrams.java
@@ -3,35 +3,30 @@ class anagrams {
     static int counter = 0;
     public static void main(String[] args) {
 
-        String str = "abc";
+        String str = "abac";
 
         char[] c = str.toCharArray();
 
-        for(int i = 0; i<c.length; i++) {
+        anagram(c, 0);
 
-            swap(c, 0, i);
-            switchchars(c, 1);
-            swap(c, 0, i);
-        }
-
-        System.out.println("Total anagrams:"+ counter);
+        System.out.println("Total iterations:"+ counter);
     }
 
-    static void switchchars(char[] c, int currPos) {
-        counter++;
-
-        String str = String.valueOf(c);
-        System.out.println(str);
-        
-        if(currPos == c.length-1) return;
-
-        for(int i = currPos+1; i< c.length; i++) {
-
-            swap(c, currPos, i);
-            switchchars(c, currPos+1);
-            swap(c, currPos, i);
+    static void anagram(char[] temp, int p1) {
+        if(p1== temp.length-1) {
+            
+            System.out.println(String.valueOf(temp));
+            return;
         }
 
+        for(int i=p1; i<temp.length; i++) {
+
+            if(p1!=i && temp[i] == temp[p1]) continue;
+            counter++;
+            swap(temp, p1, i);
+            anagram(temp, p1+1);
+            swap(temp, p1, i);
+        }
     }
 
     static void swap(char[] c, int a, int b) {
