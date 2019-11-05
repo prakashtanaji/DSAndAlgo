71e01b69ffb46941e5803522945087f971d2c6b2
sea\gtanaji <gtanaji@expedia.com>
2019-11-05 15:25:53 -0800

    Continuous subarrays sum equals  to K

--- dailycode/subarraysequalstok.java
+++ dailycode/subarraysequalstok.java
@@ -1,12 +1,18 @@
+import java.util.*;
+
 class subarraysequalstok {
     public static void main(String[] args) {
-        System.out.prinln("subarraysequalstok");
+        System.out.println("subarraysequalstok");
 
         int[] arr = new int[]{3,4,2,5,3,3,1};
         int k =7;
 
         System.out.println("Finding the count of subarray equals K using method 1:");
-        int count = checkAtEachElementMethod1(int[] arr, int k);
+        int count = checkAtEachElementMethod1(arr, k);
+        System.out.println(" Count of subarrays:" + count);
+
+        System.out.println("Finding the count of subarray equals K using method 2:");
+        count = checkAtEachElementMethod1(arr, k);
         System.out.println(" Count of subarrays:" + count);
     }
 
@@ -25,6 +31,37 @@ class subarraysequalstok {
                     break;
                 }
             }
+        }
+
+        System.out.println("    Accessed element times:"+ elementAccessCounter);
+
+        return countOfSubArray;
+    }
+
+    static int curSumApproachMethod2(int[] arr, int k) {
+
+        int elementAccessCounter = 0;
+
+        int countOfSubArray = 0;
+
+        Map<Integer, Integer> mp = new HashMap<Integer, Integer>();
+        int currSum = 0;
+
+        for(int i=0; i<arr.length; i++) {
+
+            elementAccessCounter++;
+
+            currSum+= arr[i];
+
+            if(currSum == k) {
+                countOfSubArray++;
+            }
+
+            if(mp.containsKey(currSum - k)) {
+                countOfSubArray++;
+            }
+
+            mp.put(currSum, mp.getOrDefault(currSum, 0)+1);
         }
 
         System.out.println("    Accessed element times:"+ elementAccessCounter);
