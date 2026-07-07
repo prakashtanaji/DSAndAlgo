1ec34cf5f418e7a77397654cede6e1bb7ccabca5
Dheeraj Tanaji <stevealex20000000@gmail.com>
2026-07-07 11:45:45 -0700

    fixed trapping rain water

--- dailycode/arrays/trappingRainWater.py
+++ dailycode/arrays/trappingRainWater.py
@@ -22,4 +22,27 @@ def trappingRainWater(height):
 
     return total_water
 
-print(trappingRainWater([0,1,0,2,1,0,1,3,2,1,2,1]))
+def trappingRainWater2(height):
+    if not height: return 0
+
+    left, right = 0, len(height) -1
+    left_max, right_max = height[left], height[right]
+
+    total_water = 0
+
+    while left < right:
+        if left_max < right_max:
+
+            total_water += left_max;
+            left +=1
+            if height[left] > left_max:
+                left_max = height[left]
+        else:
+            total_water += right_max;
+            right -=1
+            if height[right] > right_max:
+                right_max = height[right]
+
+    return total_water
+
+print(trappingRainWater2([0,1,0,2,1,0,1,3,2,1,2,1]))
